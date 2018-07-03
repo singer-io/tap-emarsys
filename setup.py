1017f6b98e27ba1a71b559911f33ed0c503c5538
Kyle Allan <kyle@stitchdata.com>
2018-07-03 11:28:42 -0400

    bump to 0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-emarsys",
-    version="0.3.0",
+    version="0.3.1",
     description="Singer.io tap for extracting data from the Emarsys API",
     author="Stitch",
     url="http://singer.io",
